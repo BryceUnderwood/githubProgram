8db56a1245fa58c49fa2dca84c8df04690984f2d
BDU4@students.ptcollege.edu <>
2020-02-04 20:43:10 -0500

    this is the first commit

---
+++ src/githubprogram/GithubProgram.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubprogram;
+
+/**
+ *
+ * @author ubryc
+ */
+public class GithubProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Underwood();
+    }
+    
+    public static void Underwood(){
+        System.out.println("Bryce Underwood!!");
+    }
+    
+}
